2b96c25d2c649ac2df882f73b36b263bb755795f
Ari Apriansyah <77948753+aprianz042@users.noreply.github.com>
2024-12-05 17:06:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -360,8 +360,7 @@ def eksekusi_utama(prompt):
             st.error("Siap salah! Saya melakukan kesalahan, mohon izin untuk mengulangi kembali atau ganti perintah", icon="🙏")
             kesalahan()
 
-    if cek_perintah(prompt, p_grafik):    
-    #elif cek_perintah(prompt, p_grafik):
+    elif cek_perintah(prompt, p_grafik):
         try:
             q,d,fi,disc,s = run_task_grafik(prompt, aturan)
             with st.chat_message("assistant", avatar=BOT_AVATAR):
