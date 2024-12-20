68efad1cdcf3b705c245b1922adbfe92ca3300cc
Ari Apriansyah <77948753+aprianz042@users.noreply.github.com>
2024-12-20 17:29:50 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+main_page = st.Page("chat.py", title="Main App", icon=":material/add_circle:")
+delete_page = st.Page("train_knn.py", title="Classification", icon=":material/delete:")
+
+pg = st.navigation([main_page, 
+                    delete_page])
+
+
+st.set_page_config(page_title="PNS BOT", 
+                   page_icon=":material/edit:")
+
+pg.run()
