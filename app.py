e8fd791b55c1709e19a7b6cf580900b7762e643a
Ari Apriansyah <77948753+aprianz042@users.noreply.github.com>
2024-12-30 02:58:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 
-main_page = st.Page("main.py", title="Main App", icon=":material/add_circle:")
-delete_page = st.Page("train_knn.py", title="Classification", icon=":material/delete:")
+#main_page = st.Page("chat.py", title="Main App", icon=":material/add_circle:")
+
+main_page = st.Page("main.py", title="🤖 Main App")
+knn = st.Page("knn.py", title="♻️ Prompt Classification")
+backend = st.Page("backend.py", title="⚙️ Back-End")
+guide = st.Page("guide.py", title="📔 Prompt Guide")
 
 pg = st.navigation([main_page, 
-                    delete_page])
-
+                    knn,
+                    backend,
+                    guide])
 
 st.set_page_config(page_title="PNS BOT", 
-                   page_icon=":material/edit:")
-
+                   page_icon="🤖",
+                   layout="wide")
 pg.run()
